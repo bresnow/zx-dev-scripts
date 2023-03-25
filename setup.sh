3959eb4d2568cf72e48af5c4c0f614a3afa84a61
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2023-03-25 08:26:30 +0000

    push
    Signed-off-by: βτεsηΘω <45741877+bresnow@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 echo "Setting up curl zsh nvm!"
 sudo apt install curl zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo "export NVM_DIR="$HOME/.nvm" >> ~/.zshrc
+echo "[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"" >> ~/.zshrc
+echo "[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"" >> ~/.zshrc
+zsh
 nvm install node
 npm i -g npm@latest
 npm install -g zx
